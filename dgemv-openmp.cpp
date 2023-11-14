0c330390fc5f96651e5171abd1798914a6a3483a
Parallels <hchen31@sfsu.edu>
2023-11-14 01:11:27 -0800

    fix

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -14,17 +14,17 @@ const char* dgemv_desc = "OpenMP dgemv.";
 
 void my_dgemv(int n, double* A, double* x, double* y) {
 
-   #pragma omp parallel
-   {
-      int nthreads = omp_get_num_threads();
-      int thread_id = omp_get_thread_num();
-      printf("my_dgemv(): Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
-   }
-
-   for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            y[i] += A[i * n + j] * x[j];
+   //#pragma omp parallel
+   //{
+      //int nthreads = omp_get_num_threads();
+      //int thread_id = omp_get_thread_num();
+      //printf("my_dgemv(): Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
+   //}
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                y[i] += A[i * n + j] * x[j];
+            }
         }
-    }
 }
 
