f08268ba636d593cd50541ec40c11e78841c16cd
Parallels <hchen31@sfsu.edu>
2023-11-14 01:03:50 -0800

    fix

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -8,12 +8,9 @@ const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of basic matrix multiply
-   #pragma omp simd
    for (int i = 0; i < n; i++) {
-      double temp = 0.0;
       for (int j = 0; j < n; j++) {
-         temp += A[i * n + j] * x[j];
+         y[i] += A[i * n + j] * x[j];
       }
-      y[i] += temp;
-   }
+    }
 }
