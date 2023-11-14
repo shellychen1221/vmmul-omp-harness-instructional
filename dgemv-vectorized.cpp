7ae79edd1b9b9ad433c82e0898debe2c3fa7812e
Parallels <hchen31@sfsu.edu>
2023-11-14 00:47:48 -0800

    fix

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -8,16 +8,22 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  * On exit, A and X maintain their input values.
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
-   // Ensure proper alignment for efficient memory access
-    alignas(16) double acc[4] = {0.0};  // Assuming AVX width of 4
+    alignas(32) double acc[4] = {0.0};  // Assuming AVX width of 4
 
     for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            acc[0] += A[i * n + j] * x[j];
+        __m256d sum = _mm256_setzero_pd();
+
+        for (int j = 0; j < n; j += 4) {
+            __m256d a_vec = _mm256_load_pd(&A[i * n + j]);
+            __m256d x_vec = _mm256_loadu_pd(&x[j]);
+            sum = _mm256_add_pd(sum, _mm256_mul_pd(a_vec, x_vec));
         }
 
-        // Accumulate the results into the output vector y
-        y[i] += acc[0];
-        acc[0] = 0.0;  // Reset accumulator for the next iteration
+        // Horizontal sum using hadd
+        sum = _mm256_hadd_pd(sum, sum);
+        sum = _mm256_hadd_pd(sum, sum);
+
+        // Store the result in the output vector y
+        y[i] += _mm256_cvtsd_f64(sum);
     }
 }
