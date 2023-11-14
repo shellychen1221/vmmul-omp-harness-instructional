9367e770257782c8c9dbedd870a78e2a6a0c2dd7
Parallels <hchen31@sfsu.edu>
2023-11-14 00:44:44 -0800

    fix

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -11,21 +11,23 @@ void my_dgemv(int n, double* A, double* x, double* y) {
     alignas(32) double acc[4] = {0.0};  // Assuming AVX width of 4
 
     for (int i = 0; i < n; ++i) {
+        // Use a temporary pointer for better access pattern
         double* A_row = &A[i * n];
-        __m256d acc_vec = _mm256_setzero_pd();  // Initialize accumulator vector
 
         for (int j = 0; j < n; j += 4) {
-            __m256d A_vec = _mm256_loadu_pd(&A_row[j]);
-            __m256d x_vec = _mm256_loadu_pd(&x[j]);
-            acc_vec = _mm256_add_pd(acc_vec, _mm256_mul_pd(A_vec, x_vec));
-        }
+            // Load 4 elements at a time using AVX
+            __m256d A_vec = _mm256_load_pd(&A_row[j]);
+            __m256d x_vec = _mm256_load_pd(&x[j]);
 
-        // Horizontal sum of the accumulator vector
-        __m128d acc128 = _mm_add_pd(_mm256_extractf128_pd(acc_vec, 0), _mm256_extractf128_pd(acc_vec, 1));
-        acc[0] += acc128[0];
-        acc[1] += acc128[1];
+            // Multiply and accumulate
+            __m256d result = _mm256_mul_pd(A_vec, x_vec);
+            acc[0] += result[0];
+            acc[1] += result[1];
+            acc[2] += result[2];
+            acc[3] += result[3];
+        }
     }
 
     // Accumulate the results into the output vector y
-    y[0] += acc[0] + acc[1];
+    y[0] += acc[0] + acc[1] + acc[2] + acc[3];
 }
