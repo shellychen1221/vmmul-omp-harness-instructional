62eea0c1d79e2a9cf05ccc6c5942662b0f40d4bd
Parallels <hchen31@sfsu.edu>
2023-11-14 00:33:50 -0800

    fix

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -8,16 +8,26 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  * On exit, A and X maintain their input values.
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
-   // Ensure proper alignment for efficient memory access
-    alignas(16) double acc[4] = {0.0};  // Assuming AVX width of 4
+    alignas(32) double acc[4] = {0.0};  // Assuming AVX width of 4
 
     for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            acc[0] += A[i * n + j] * x[j];
-        }
+        // Use a temporary pointer for better access pattern
+        double* A_row = &A[i * n];
 
-        // Accumulate the results into the output vector y
-        y[i] += acc[0];
-        acc[0] = 0.0;  // Reset accumulator for the next iteration
+        for (int j = 0; j < n; j += 4) {
+            // Load 4 elements at a time using AVX
+            __m256d A_vec = _mm256_load_pd(&A_row[j]);
+            __m256d x_vec = _mm256_load_pd(&x[j]);
+
+            // Multiply and accumulate
+            __m256d result = _mm256_mul_pd(A_vec, x_vec);
+            acc[0] += result[0];
+            acc[1] += result[1];
+            acc[2] += result[2];
+            acc[3] += result[3];
+        }
     }
+
+    // Accumulate the results into the output vector y
+    y[0] += acc[0] + acc[1] + acc[2] + acc[3];
 }
