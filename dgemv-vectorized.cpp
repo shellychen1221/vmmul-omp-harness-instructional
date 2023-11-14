0ae9b8a8f153c8636f8255bbaabaa2cf6dde95b4
Parallels <hchen31@sfsu.edu>
2023-11-14 01:13:04 -0800

    fix

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -8,6 +8,7 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of basic matrix multiply
+   #pragma omp simd
    for (int i = 0; i < n; i++) {
       for (int j = 0; j < n; j++) {
          y[i] += A[i * n + j] * x[j];
