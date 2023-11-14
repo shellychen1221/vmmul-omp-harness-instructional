34e377583df67878117d1f52e4bfa7ffe4178fe4
Parallels <hchen31@sfsu.edu>
2023-11-14 00:36:44 -0800

    fix

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -11,25 +11,26 @@ void my_dgemv(int n, double* A, double* x, double* y) {
     alignas(32) double acc[4] = {0.0};  // Assuming AVX width of 4
 
     for (int i = 0; i < n; ++i) {
-        // Use a temporary pointer for better access pattern
+
         double* A_row = &A[i * n];
 
+        // Unroll the loop to better facilitate vectorization
         for (int j = 0; j < n; j += 4) {
-            // Load 4 elements at a time using AVX
-            __m256d A_vec = _mm256_loadu_pd(&A_row[j]);  // Use loadu to handle potential misalignment
-
-            // Load 4 elements from x using AVX
+            __m256d A_vec = _mm256_loadu_pd(&A_row[j]);
             __m256d x_vec = _mm256_loadu_pd(&x[j]);
 
-            // Multiply and accumulate
             __m256d result = _mm256_mul_pd(A_vec, x_vec);
             __m128d result128 = _mm_add_pd(_mm256_extractf128_pd(result, 0), _mm256_extractf128_pd(result, 1));
 
             acc[0] += result128[0];
             acc[1] += result128[1];
         }
-    }
 
-    // Accumulate the results into the output vector y
-    y[0] += acc[0] + acc[1];
+        // Accumulate the results into the output vector y
+        y[i] += acc[0] + acc[1];
+
+        // Reset accumulator for the next iteration
+        acc[0] = 0.0;
+        acc[1] = 0.0;
+    }
 }
