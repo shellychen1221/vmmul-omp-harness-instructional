bcfedee9dee977405432c36546f7b9134a6f687e
Parallels <hchen31@sfsu.edu>
2023-11-13 23:09:39 -0800

    fix

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -22,7 +22,6 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    }
 
    for (int i = 0; i < n; i++) {
-        y[i] = 0.0;
         for (int j = 0; j < n; j++) {
             y[i] += A[i * n + j] * x[j];
         }
